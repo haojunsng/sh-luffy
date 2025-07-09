737d1116f41a1f57e9bd0215c5e93e7b832ef23d
haojunsng <snghaojun18@gmail.com>
2025-07-10 01:07:17 +0800

    feat: Implement subscription feature

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,29 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-interface SubscribeRequestBody {
-  email: string
-}
+// interface SubscribeRequestBody {
+//   email: string
+// }
+
+// export async function POST(req: NextRequest) {
+//   const { email } = (await req.json()) as SubscribeRequestBody
+
+//   const WORKER_SECRET_TOKEN = process.env.WORKER_SECRET_TOKEN
+//   if (!WORKER_SECRET_TOKEN) {
+//     return NextResponse.json({ error: 'Missing secret token' }, { status: 500 })
+//   }
+
+//   const workerResponse = await fetch(
+//     'https://silent-truth-cf96.snghaojun18.workers.dev/subscribe',
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${WORKER_SECRET_TOKEN}`,
+//       },
+//       body: JSON.stringify({ email }),
+//     }
+//   )
+
+//   const data = await workerResponse.json()
+//   return NextResponse.json(data)
+// }
 
 export async function POST(req: NextRequest) {
-  const { email } = (await req.json()) as SubscribeRequestBody
-
-  const WORKER_SECRET_TOKEN = process.env.WORKER_SECRET_TOKEN
-  if (!WORKER_SECRET_TOKEN) {
-    return NextResponse.json({ error: 'Missing secret token' }, { status: 500 })
-  }
-
-  const workerResponse = await fetch(
-    'https://silent-truth-cf96.snghaojun18.workers.dev/subscribe',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${WORKER_SECRET_TOKEN}`,
-      },
-      body: JSON.stringify({ email }),
-    }
-  )
-
-  const data = await workerResponse.json()
-  return NextResponse.json(data)
+  return NextResponse.json({ message: 'POST handler works!' })
 }
