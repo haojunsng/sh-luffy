edbab3771a5402f8f862083fa4e74537b5dd6889
haojunsng <snghaojun18@gmail.com>
2025-07-10 01:12:25 +0800

    feat: Implement subscription feature

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const output = 'export'
 const basePath = process.env.BASE_PATH || undefined
 const unoptimized = true
 
@@ -14,7 +13,6 @@ const unoptimized = true
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
-    output,
     basePath,
     reactStrictMode: true,
     trailingSlash: false,
