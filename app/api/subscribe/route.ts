fcb3d917c282b9e81810a62ca019fbe973a3203f
haojunsng <snghaojun18@gmail.com>
2025-07-10 11:38:29 +0800

    fix: Wrap splitChunks into isServer block

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,38 +1,31 @@
-// export const runtime = 'edge'
-
-// import { NextRequest, NextResponse } from 'next/server'
-
-// interface SubscribeRequestBody {
-//   email: string
-// }
-
-// export async function POST(req: NextRequest) {
-//   const { email } = (await req.json()) as SubscribeRequestBody
-
-//   const WORKER_SECRET_TOKEN = process.env.WORKER_SECRET_TOKEN
-//   if (!WORKER_SECRET_TOKEN) {
-//     return NextResponse.json({ error: 'Missing secret token' }, { status: 500 })
-//   }
-
-//   const workerResponse = await fetch(
-//     'https://silent-truth-cf96.snghaojun18.workers.dev/subscribe',
-//     {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: `Bearer ${WORKER_SECRET_TOKEN}`,
-//       },
-//       body: JSON.stringify({ email }),
-//     }
-//   )
-
-//   const data = await workerResponse.json()
-//   return NextResponse.json(data)
-// }
 export const runtime = 'edge'
 
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST() {
-  return NextResponse.json({ message: 'ok' })
+interface SubscribeRequestBody {
+  email: string
+}
+
+export async function POST(req: NextRequest) {
+  const { email } = (await req.json()) as SubscribeRequestBody
+
+  const WORKER_SECRET_TOKEN = process.env.WORKER_SECRET_TOKEN
+  if (!WORKER_SECRET_TOKEN) {
+    return NextResponse.json({ error: 'Missing secret token' }, { status: 500 })
+  }
+
+  const workerResponse = await fetch(
+    'https://silent-truth-cf96.snghaojun18.workers.dev/subscribe',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${WORKER_SECRET_TOKEN}`,
+      },
+      body: JSON.stringify({ email }),
+    }
+  )
+
+  const data = await workerResponse.json()
+  return NextResponse.json(data)
 }
