3b9f970bde7f88a08f6b16374e02a5f4c00771ab
haojunsng <snghaojun18@gmail.com>
2025-07-12 16:11:14 +0800

    fix: Fix blog post pagination

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -1,11 +1,12 @@
 import List from '@/components/List'
 import { genPageMetadata } from 'app/seo'
-import { getBlogPageData } from './utils'
+import { getBlogPageData } from '../../utils'
 
 export const metadata = genPageMetadata({ title: 'Blog' })
 
-const page = async () => {
-  const { posts, initialDisplayPosts, pagination } = getBlogPageData(1)
+const page = async ({ params }: { params: { page: string } }) => {
+  const pageNumber = parseInt(params.page as string)
+  const { posts, initialDisplayPosts, pagination } = getBlogPageData(pageNumber)
 
   return (
     <List
